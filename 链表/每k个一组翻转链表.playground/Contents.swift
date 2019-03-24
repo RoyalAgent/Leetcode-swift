9be78261feebf8d00c8b036ae4c547cf736f8621
RoyalAgent <libochao12@126.com>
2019-03-24 16:31:22 +0800

    v1.0

---
+++ 链表/每k个一组翻转链表.playground/Contents.swift
@@ -0,0 +1,44 @@
+/**
+ * Question Link: https://leetcode.com/problems/reverse-nodes-in-k-group/
+ * Primary idea: Reverse one group during one loop. Construct a reversed group from back to forth.
+ * Time Complexity: O(n), Space Complexity: O(1)
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+/*
+ Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ 
+ k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ 
+ Example:
+ 
+ Given this linked list: 1->2->3->4->5
+ 
+ For k = 2, you should return: 2->1->4->3->5
+ 
+ For k = 3, you should return: 3->2->1->4->5
+ 
+ Note:
+ 
+ Only constant extra memory is allowed.
+ You may not alter the values in the list's nodes, only nodes itself may be changed.
+ */
+import UIKit
+
+class ListNode{
+    var val:Int
+    var next:ListNode?
+    init(_ val:Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
